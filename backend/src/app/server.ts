8f92f311b8c07c63f313d275060bd14f46de0102
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-05-13 01:32:32 -0300

    config(backend): Add dotenv

--- backend/src/app/server.ts
+++ backend/src/app/server.ts
@@ -4,6 +4,6 @@ import { router } from "./router";
 const app: Application = express();
 
 app.use(express.json());
-app.use('/api',router);
+app.use('/api', router);
 
 export { app }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,19 @@
+import express from "express";
+import path from "path";
+import { config } from "dotenv";
 import { app as server } from "./app/server";
+import { renderFile } from "ejs";
 
-server.listen(3333, () => console.log('Listening on 3333 port'));
+config();
+
+const DEV = process.env['SERVER_DEV'] || false;
+const PORT = DEV ? 3333 : 80;
+if (!DEV) {
+    server.engine('html', renderFile);
+    server.use(express.static(path.join(__dirname,  '../../frontend/build')));
+    server.get('/', (req, res) => {
+        res.render('index.html');
+    })
+}
+
+server.listen(PORT, () => console.log(`Listening on ${PORT} port`));
